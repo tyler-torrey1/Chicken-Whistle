0837f5f7a4ae4b7175f96eaafa0a89ee750e51ce
Julian Druzdzel <j.druzdzel@gmail.com>
2026-03-06 23:06:48 -0500

    bettered but still bad jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,5 @@
-using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
@@ -10,18 +10,20 @@ public class PlayerController : MonoBehaviour
     new public BoxCollider2D collider { get; private set; }
 
     // horizontal control
-    [SerializeField] private float moveAcc = 50.0f;
-    [SerializeField] private float moveMaxVel = 5f;
-    [SerializeField] private float jumpHeight;
+    [SerializeField, Min(0)] private float moveAcc = 50.0f;
+    [SerializeField, Min(0)] private float moveMaxVel = 5f;
+    [SerializeField, Min(0)] private float maxJumpHeight;
 
 
     // vertical control state
     [SerializeField] private bool isGrounded;
+    [SerializeField] private float jumpLaunchDuration = .5f; // for max jump, hold
+    StopWatch jumpWatch;
+    bool isJumping;
 
     // general state
-    private float jumpAcc;
-    private Vector2 moveInput;
-    bool isJumping;
+    private float jumpVel => Mathf.Sqrt(2f * Mathf.Abs(Physics2D.gravity.y / (Time.fixedDeltaTime * Time.fixedDeltaTime)) * maxJumpHeight); // should set in Awake and cached
+    private Vector2 moveDirection;
 
     // data cache
 
@@ -33,12 +35,15 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
-        jumpAcc = 2f * jumpHeight * Mathf.Abs(Physics2D.gravity.y);
+
+        jumpWatch = new StopWatch();
+        //jumpVel = ; should be cached
+
     }
 
     public void SetMoveInput(Vector2 dir)
     {
-        moveInput = dir;
+        moveDirection = dir;
     }
 
     void Update()
@@ -46,80 +51,83 @@ public class PlayerController : MonoBehaviour
         // get direction of player
 
         // flip sprite based on horizontal movement
-        if (moveInput.x > 0)
+        if (moveDirection.x > 0)
         {
             spriteRenderer.flipX = false;
         }
-        else if (moveInput.x < 0)
+        else if (moveDirection.x < 0)
         {
             spriteRenderer.flipX = true;
         }
     }
 
+
     // Update is called once per frame
     void FixedUpdate()
     {
         isGrounded = CheckIfGrounded();
-        if (isJumping && !isGrounded)
-        {
-            isJumping = false;
-        }
-        MovePlayer();
+        HandleMovement(moveDirection);
+        HandleJump();
+        HandleAnimator();
     }
 
-    public void HandleInput(Vector2 direction)
+    /**
+     * Side to side movement
+     */
+    private void HandleMovement(Vector2 direction)
     {
-        float inputDirHorz = Mathf.Approximately(direction.x, 0f) ? 0f : Mathf.Sign(direction.x);
-        float inputDirVert = Mathf.Approximately(direction.y, 0f) ? 0f : Mathf.Sign(direction.y);
+        float inputHorizontal = Mathf.Approximately(direction.x, 0f) ? 0f : Mathf.Sign(direction.x);
 
-        float currHorz = body.linearVelocity.x;
-        float currVert = body.linearVelocity.y;
+        float currVelHorizontal = body.linearVelocity.x;
 
-        float desiredVelHorz = currHorz;
+        float resolvedVelHorizontal = currVelHorizontal;
 
         // Instant stop
-        if (Mathf.Approximately(inputDirHorz, 0f) || // no input
-            (!Mathf.Approximately(currHorz, 0f) && Mathf.Sign(inputDirHorz) != Mathf.Sign(currHorz))) // u-turn
+        if (Mathf.Approximately(inputHorizontal, 0f) || // no input
+            (!Mathf.Approximately(currVelHorizontal, 0f) && Mathf.Sign(inputHorizontal) != Mathf.Sign(currVelHorizontal))) // u-turn
         {
-            desiredVelHorz = 0f;
+            resolvedVelHorizontal = 0f;
         }
-        desiredVelHorz += inputDirHorz * moveAcc * Time.fixedDeltaTime;
-        desiredVelHorz = Mathf.Sign(desiredVelHorz) * Mathf.Min(Mathf.Abs(desiredVelHorz), moveMaxVel);
+        resolvedVelHorizontal += inputHorizontal * moveAcc * Time.fixedDeltaTime;
+        resolvedVelHorizontal = Mathf.Sign(resolvedVelHorizontal) * Mathf.Min(Mathf.Abs(resolvedVelHorizontal), moveMaxVel);
 
-        // Jump
-        if (!isJumping && isGrounded && inputDirVert > 0)
-        {
-            body.AddForceY(jumpAcc * body.mass);
-            isJumping = true;
-        }
+        float resolvedX = resolvedVelHorizontal;
 
-        float resolvedX = desiredVelHorz;
-        float resolvedY = currVert;
-
-        body.linearVelocity = new Vector2(resolvedX, resolvedY);
+        body.linearVelocity = new Vector2(resolvedX, body.linearVelocity.y);
     }
 
-    private void MovePlayer()
+    /**
+     * If we are in the process of jumping, we will accelerate
+     * for jumpLaunchDuration to approximately reach jumpHeight height.
+     */
+    private void HandleJump()
     {
-        // Move Player
-        HandleInput(moveInput);
+        if (isJumping && jumpWatch <= jumpLaunchDuration)
+        {
+            // Over jumpLaunchDuration, apply a total of jumpVel acceleration
+            float jumpAcc = jumpVel * (Time.fixedDeltaTime / Mathf.Max(Time.fixedDeltaTime, jumpLaunchDuration));
+            body.AddForceY(jumpAcc * body.mass);
+        }
+    }
 
-        if (moveInput.x != 0 || moveInput.y != 0)
+    private void HandleAnimator()
+    {
+        if (moveDirection.x != 0 || moveDirection.y != 0)
         {
             animator.SetBool("isMoving", true);
-            if (moveInput.x != 0)
+            if (moveDirection.x != 0)
             {
                 animator.SetBool("isSideways", true);
                 animator.SetBool("isForward", false);
                 animator.SetBool("isBackward", false);
             }
-            if (moveInput.y < 0)
+            if (moveDirection.y < 0)
             {
                 animator.SetBool("isSideways", false);
                 animator.SetBool("isForward", true);
                 animator.SetBool("isBackward", false);
             }
-            if (moveInput.y > 0)
+            if (moveDirection.y > 0)
             {
                 animator.SetBool("isSideways", false);
                 animator.SetBool("isForward", false);
@@ -155,12 +163,31 @@ public class PlayerController : MonoBehaviour
         return false;
     }
 
- /**
+    /**
      * Referenced by the Player Input to pass move commands
      */
     public void MoveInput(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        moveDirection = context.ReadValue<Vector2>();
+    }
+    
+    /**
+     * Referenced by the Player Input to pass jump command.
+     * Value comes in as 1 (pressed) or 0 (unpressed).
+     */
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        float jump = context.ReadValue<float>();
+
+        if (jump > 0f && isGrounded && isJumping == false)
+        {
+            jumpWatch.Start();
+            isJumping = true;
+        }
+        else if (jump <= 0f)
+        {
+            isJumping = false;
+        }
     }
 
     public void OnDrawGizmos()
