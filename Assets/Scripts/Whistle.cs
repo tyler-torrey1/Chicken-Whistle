1cb44c3a090a03bd681cd904c7927a7393b2f6fa
Tyler <tylertorrey16@gmail.com>
2026-03-07 19:18:35 -0500

    Starts adding whistle, adjusts scene for pixels (bg 512x288, ortho size to 4.5, pixel perfect camera added), fixes lighting of player character

---
+++ Assets/Scripts/Whistle.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Whistle : MonoBehaviour {
+    [SerializeField]
+    private float whistleRadius = 2.5f;
+
+    [SerializeField]
+    private float freezeTime = 5f;
+
+    [SerializeField]
+    private Rigidbody2D _rb;
+    void Awake() {
+        this._rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    public void BlowWhistle(InputAction.CallbackContext context) {
+        if (!context.performed) {
+            return;
+        }
+
+        Debug.Log("Blowing the whistle!");
+        Collider2D[] nearbyColliders = Physics2D.OverlapCircleAll(this._rb.worldCenterOfMass, this.whistleRadius);
+        foreach (Collider2D collider in nearbyColliders) {
+            Freezeable freezeable = collider.GetComponent<Freezeable>();
+            freezeable.Freeze(this.freezeTime);
+        }
+    }
+    public void OnDrawGizmos() {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(this._rb.worldCenterOfMass, this.whistleRadius);
+    }
+}
