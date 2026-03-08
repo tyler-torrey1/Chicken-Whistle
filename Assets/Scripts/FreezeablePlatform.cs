b23b4f5ff49f26ec8c533d5d2535725a7f2055c0
Tyler <tylertorrey16@gmail.com>
2026-03-08 01:16:35 -0500

    Adds freezeable wall, wooden platform. Adds sprites to freezeable platform

--- Assets/Scripts/FreezeablePlatform.cs
+++ Assets/Scripts/FreezeablePlatform.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 public class FreezeablePlatform : Freezeable {
 
     private SpriteRenderer _renderer;
-    private
+
+    [SerializeField]
+    private Sprite _frozenSprite;
+
+    [SerializeField]
+    private Sprite _nonFrozenSprite;
     void Start() {
-        this._renderer = this.GetComponent<SpriteRenderer>();
-        this.gameObject.layer = LayerMask.NameToLayer(layerName: "Non-Collider");
+        _renderer = GetComponent<SpriteRenderer>();
+        gameObject.layer = LayerMask.NameToLayer(layerName: "Non-Collider");
+        _renderer.sprite = _nonFrozenSprite;
     }
 
     // Update is called once per frame
     public override void Freeze(float freezeTime) {
         // Set Collider to Colliding Layer
-        this.gameObject.layer = LayerMask.NameToLayer("Collider");
         base.Freeze(freezeTime);
+        gameObject.layer = LayerMask.NameToLayer("Collider");
+        _renderer.sprite = _frozenSprite;
     }
     public override void Unfreeze() {
         // Set Collider to Uncolliding Layer
-        this.gameObject.layer = LayerMask.NameToLayer(layerName: "Non-Collider");
         base.Unfreeze();
+        gameObject.layer = LayerMask.NameToLayer(layerName: "Non-Collider");
+        _renderer.sprite = _nonFrozenSprite;
     }
 }
