3d97e56c4135a33de0644f4f3067e137483ef128
Tyler <tylertorrey16@gmail.com>
2026-03-07 21:24:06 -0500

    Adds cooldown to whistle

--- Assets/Scripts/Whistle.cs
+++ Assets/Scripts/Whistle.cs
@@ -2,31 +2,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Whistle : MonoBehaviour {
     [SerializeField]
-    private float whistleRadius = 2.5f;
+    private float _whistleRadius = 2.5f;
 
     [SerializeField]
-    private float freezeTime = 5f;
+    private float _freezeTime = 5f;
 
     [SerializeField]
     private Rigidbody2D _rb;
+
+    [SerializeField]
+    private float _whistleCooldown = 5.5f;
+    private float _nextWhistleTime = 0f;
+
     void Awake() {
-        this._rb = this.GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     public void BlowWhistle(InputAction.CallbackContext context) {
-        if (!context.performed) {
+        if (!context.performed || _nextWhistleTime > Time.time) {
             return;
         }
 
         Debug.Log("Blowing the whistle!");
-        Collider2D[] nearbyColliders = Physics2D.OverlapCircleAll(this._rb.worldCenterOfMass, this.whistleRadius);
+        Collider2D[] nearbyColliders = Physics2D.OverlapCircleAll(_rb.worldCenterOfMass, _whistleRadius);
         foreach (Collider2D collider in nearbyColliders) {
             Freezeable freezeable = collider.GetComponent<Freezeable>();
-            freezeable?.Freeze(this.freezeTime);
+            freezeable?.Freeze(_freezeTime);
         }
+        _nextWhistleTime = Time.time + _whistleCooldown;
     }
     public void OnDrawGizmos() {
         Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(this._rb.worldCenterOfMass, this.whistleRadius);
+        Gizmos.DrawWireSphere(_rb.worldCenterOfMass, _whistleRadius);
     }
 }
